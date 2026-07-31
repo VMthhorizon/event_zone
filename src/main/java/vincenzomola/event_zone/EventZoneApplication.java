16ab3742f7f9bfa8172402abf74aeea7f9802937
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-31 15:57:46 +0200

    Initial commit

---
+++ src/main/java/vincenzomola/event_zone/EventZoneApplication.java
@@ -0,0 +1,13 @@
+package vincenzomola.event_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzomola/event_zone/EventZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.event_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

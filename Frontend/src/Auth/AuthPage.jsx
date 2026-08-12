6d7db2d270da5affa9c94263bca4dda7e16560d2
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-08-13 00:33:13 +0200

    New BG video and configuration env file

--- Frontend/src/Auth/AuthPage.jsx
+++ Frontend/src/Auth/AuthPage.jsx
@@ -1,18 +1,16 @@
-import { Button, Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "./AuthPage.css";
 
 function AuthPage() {
   const navigate = useNavigate();
   return (
-    <Container fluid>
-      <div className="video-container">
-        <video className="bg-video" muted playsInline loop>
-          <source src="video-bg.mp4" type="video/mp4"></source>
-        </video>
-      </div>
+    <>
+      <video className="bg-video" autoPlay muted playsInline loop>
+        <source src="video-bg.mp4" type="video/mp4"></source>
+      </video>
       <Button onClick={() => navigate("/home")}>TEST NOT FOUND</Button>
-    </Container>
+    </>
   );
 }
 

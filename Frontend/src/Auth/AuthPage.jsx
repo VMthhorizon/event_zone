116b59c3a676be302d6f34fb437585169b301c1a
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-08-13 03:30:22 +0200

    Register/Sign in buttons centered

--- Frontend/src/Auth/AuthPage.jsx
+++ Frontend/src/Auth/AuthPage.jsx
@@ -1,15 +1,20 @@
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import "./AuthPage.css";
 
 function AuthPage() {
-  const navigate = useNavigate();
   return (
     <>
       <video className="bg-video" autoPlay muted playsInline loop>
         <source src="video-bg.mp4" type="video/mp4"></source>
       </video>
-      <Button onClick={() => navigate("/home")}>TEST NOT FOUND</Button>
+      <div className="d-flex justify-content-center align-items-center gap-3 vh-100">
+        <Button variant="info" className="btn fs-5 bg-gradient">
+          ACCEDI
+        </Button>
+        <Button variant="primary" className="btn fs-5">
+          REGISTRATI
+        </Button>
+      </div>
     </>
   );
 }

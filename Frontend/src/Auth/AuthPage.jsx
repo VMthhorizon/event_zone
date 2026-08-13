742cdfdfa90e41658934234db4129bcde2979832
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-08-13 17:54:48 +0200

    Animazione buttons e icons nella pagina login e registrazione utente

--- Frontend/src/Auth/AuthPage.jsx
+++ Frontend/src/Auth/AuthPage.jsx
@@ -1,5 +1,7 @@
 import { Button } from "react-bootstrap";
 import "./AuthPage.css";
+import { CiLogin } from "react-icons/ci";
+import { MdAppRegistration } from "react-icons/md";
 
 function AuthPage() {
   return (
@@ -8,8 +10,17 @@ function AuthPage() {
         <source src="video-bg.mp4" type="video/mp4"></source>
       </video>
       <div className="d-flex justify-content-center align-items-center gap-3 vh-100">
-        <Button className="btn-gradient">Accedi</Button>
-        <Button className="btn-gradient">Registrati</Button>
+        <Button className="btn-gradient">
+          <div className="d-flex align-items-center gap-2">
+            Accedi <CiLogin className="login-register-icons-animation" />
+          </div>
+        </Button>
+        <Button className="btn-gradient">
+          <div className="d-flex align-items-center gap-2">
+            Registrati{" "}
+            <MdAppRegistration className="login-register-icons-animation" />
+          </div>
+        </Button>
       </div>
     </>
   );

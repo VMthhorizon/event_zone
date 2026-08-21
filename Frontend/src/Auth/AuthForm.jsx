96abdbe04452f6614d786c428638ef3afd66f04f
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-08-21 03:08:07 +0200

    Animazioni form auth con framemotion e collegamento cors

--- Frontend/src/Auth/AuthForm.jsx
+++ Frontend/src/Auth/AuthForm.jsx
@@ -3,33 +3,133 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { IoSend } from "react-icons/io5";
 import "./AuthForm.css";
+import { motion, AnimatePresence } from "framer-motion";
 
-function AuthForm() {
+function AuthForm({ authMode, setAuthMode }) {
   return (
-    <Card className="p-3 w-100 d-flex mb-3 card-auth card-form">
-      <Form>
-        <Form.Group className="mb-3" controlId="Email">
-          <Form.Label>
-            <h5>Email</h5>
-          </Form.Label>
-          <Form.Control type="email" placeholder="Inserisci la tua email" />
-          <Form.Text className="text-muted"></Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="Password">
-          <Form.Label>
-            <h5>Password</h5>
-          </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Inserisci la tua password"
-          />
-        </Form.Group>
-        <Button className="btn-gradient ">
-          <h6> Conferma</h6>
-          <IoSend className="login-register-icons-animation" />
-        </Button>
-      </Form>
-    </Card>
+    <>
+      {authMode !== "" && (
+        <Card className="p-3 d-flex mb-4 w-100 card-auth card-form ">
+          <Form>
+            <AnimatePresence initial={false}>
+              {authMode === "register" && (
+                <motion.div
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: "auto" }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  style={{ overflow: "hidden" }}
+                >
+                  <h1 className="text-center mb-4">REGISTRAZIONE</h1>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <h5>Username</h5>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Inserisci un username"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <h5>Nome</h5>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Inserisci il tuo nome"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <h5>Cognome</h5>
+                    </Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Inserisci il tuo cognome"
+                    />
+                  </Form.Group>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            {(authMode === "login" || authMode === "register") && (
+              <>
+                {authMode === "login" && (
+                  <h1 className="text-center mb-4">LOGIN</h1>
+                )}
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    <h5>Email</h5>
+                  </Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Inserisci la tua email"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-5">
+                  <Form.Label>
+                    <h5>Password</h5>
+                  </Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Inserisci la tua password"
+                  />
+                </Form.Group>
+              </>
+            )}
+
+            <AnimatePresence initial={false}>
+              {authMode === "password" && (
+                <motion.div
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: "auto" }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  style={{ overflow: "hidden" }}
+                >
+                  <h1 className="text-center mb-4">MODIFICA PASSWORD</h1>
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <h5>Vecchia Password</h5>
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Inserisci la tua password corrente"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-5">
+                    <Form.Label>
+                      <h5>Nuova Password</h5>
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Inserisci la tua nuova password"
+                    />
+                  </Form.Group>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            <div className="d-flex align-items-center ">
+              <Button className="btn-gradient">
+                <h6>Conferma</h6>
+                <IoSend className="login-register-icons-animation" />
+              </Button>
+              {authMode === "login" && (
+                <a
+                  onClick={(e) => {
+                    e.preventDefault;
+                    setAuthMode("password");
+                  }}
+                  className="text-end w-100"
+                >
+                  Recupera la tua password
+                </a>
+              )}
+            </div>
+          </Form>
+        </Card>
+      )}
+    </>
   );
 }
 
--- Frontend/src/Auth/AuthPage.jsx
+++ Frontend/src/Auth/AuthPage.jsx
@@ -1,25 +1,71 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./AuthPage.css";
 import { CiLogin } from "react-icons/ci";
 import { MdAppRegistration } from "react-icons/md";
 import AuthForm from "./AuthForm";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 function AuthPage() {
+  const [authMode, setAuthMode] = useState("");
+
   return (
     <>
       <video className="bg-video" autoPlay muted playsInline loop>
         <source src="video-bg.mp4" type="video/mp4"></source>
       </video>
       <Container className="flex-column d-flex justify-content-center align-items-center vh-100">
-        <AuthForm />
-        <Button className="btn-gradient">
-          <h6>Accedi</h6>
-          <CiLogin className="login-register-icons-animation" />
-        </Button>
-        <Button className="btn-gradient">
-          <h6>Registrati</h6>
-          <MdAppRegistration className="login-register-icons-animation" />
-        </Button>
+        <Row className="w-100 justify-content-center align-items-center">
+          <Col xs={12} md={9} lg={6}>
+            <AuthForm authMode={authMode} setAuthMode={setAuthMode} />
+            <Col className="d-flex gap-1">
+              <AnimatePresence>
+                {authMode !== "login" && (
+                  <motion.div
+                    key="btn-login"
+                    layout
+                    style={{ flex: 1, width: 0 }}
+                    initial={{ opacity: 0, flex: 0 }}
+                    animate={{ opacity: 1, flex: 1 }}
+                    exit={{ opacity: 0, flex: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                  >
+                    <Button
+                      onClick={() => {
+                        setAuthMode("login");
+                      }}
+                      className="btn-gradient btn-auth "
+                    >
+                      <h6>Accedi</h6>
+                      <CiLogin className="login-register-icons-animation" />
+                    </Button>
+                  </motion.div>
+                )}
+                {authMode !== "register" && (
+                  <motion.div
+                    key="btn-register"
+                    layout
+                    style={{ flex: 1, width: 0 }}
+                    initial={{ opacity: 0, flex: 0 }}
+                    animate={{ opacity: 1, flex: 1 }}
+                    exit={{ opacity: 0, flex: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                  >
+                    <Button
+                      onClick={() => {
+                        setAuthMode("register");
+                      }}
+                      className="btn-gradient btn-auth "
+                    >
+                      <h6>Registrati</h6>
+                      <MdAppRegistration className="login-register-icons-animation" />
+                    </Button>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </Col>
+          </Col>
+        </Row>
       </Container>
     </>
   );

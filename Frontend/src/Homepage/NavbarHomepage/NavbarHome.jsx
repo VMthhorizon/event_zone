d89b304fa450eb7f60cd81931c37957f7ede2a69
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-08-26 13:17:24 +0200

    Stato e funzione per aggiornare i filtri per categorie degli eventii

--- Frontend/src/Homepage/NavbarHomepage/NavbarHome.jsx
+++ Frontend/src/Homepage/NavbarHomepage/NavbarHome.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./NavbarHome.css";
 
 import Button from "react-bootstrap/Button";
@@ -11,8 +12,38 @@ import { ImCart } from "react-icons/im";
 import { VscSearchSparkle } from "react-icons/vsc";
 
 function NavbarHome() {
+  // Stato per gestire le checkbox relative ai filtri per categorie tramite hook
+
+  const [categories, setCategories] = useState({
+    tutti: true,
+    concerti: false,
+    festival: false,
+    cinema: false,
+    teatro: false,
+  });
+
+  // Funzione per aggiornare lo stato delel categorie
+  const handleCategoriesChange = (category) => {
+    if (category === "tutti") {
+      // Se clicco su "Tutti", tutte le altre categorie diventeranno false, deselezionando le checkbox
+      setCategories({
+        tutti: true,
+        concerti: false,
+        festival: false,
+        cinema: false,
+        teatro: false,
+      });
+    } else {
+      setCategories((prev) => ({
+        ...prev, // Mantengo le altre categorie
+        tutti: false, // Deseleziono la checkbox con "tutti"
+        [category]: !prev[category], // Inverte il valore solo della checkbox cliccata
+      }));
+    }
+  };
+
   return (
-    <Navbar className="flex-grow-1 navbar-home ">
+    <Navbar className="navbar-home ">
       <Container fluid className="px-0 flex-column align-items-center w-100">
         <div className="w-100 d-flex justify-content-between align-items-center">
           <Navbar.Brand>
@@ -27,16 +58,29 @@ function NavbarHome() {
               <NavDropdown
                 align="end"
                 title={<ImCart className="fs-4" />}
-                id="navbarScrollingDropdown"
+                id="cart-dropdown"
               >
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action3">Lista</NavDropdown.Item>
+                <NavDropdown.Item href="#action3">
+                  Prezzo Totale
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action3">Checkout</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown
                 align="end"
                 title={<CgProfile className="fs-4" />}
-                id="navbarScrollingDropdown"
+                id="profile-dropdown"
               >
-                <NavDropdown.Item href="#action4">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Profilo Personale
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Cronologia Acquisti
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Impostazioni
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Esci</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </div>
